56141358e9830ebae7ce5a62adc9157dfd29c851
Walter Vargas <waltervargas@linux.com>
2022-09-25 06:59:40 +0200

    Add words flag and runCLI  (#1)

--- cmd/counter/main.go
+++ cmd/counter/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/waltervargas/counter"
 )
 
 func main() {
-	fmt.Println(counter.Lines())
+	counter.RunCLI()
 }
--- counter_test.go
+++ counter_test.go
@@ -28,7 +28,7 @@ func TestWithInputFromArgs(t *testing.T) {
 	t.Parallel()
 	args := []string{"testdata/three_lines.txt"}
 	c, err := counter.NewCounter(
-		counter.WithInputFromArgs(args),
+		counter.FromArgs(args),
 	)
 	if err != nil {
 		t.Fatal(err)
@@ -45,13 +45,44 @@ func TestWithInputFromArgsEmpty(t *testing.T) {
 	inputBuf := bytes.NewBufferString("1\n2\n3")
 	c, err := counter.NewCounter(
 		counter.WithInput(inputBuf),
-		counter.WithInputFromArgs([]string{}),
+		counter.FromArgs([]string{}),
 	)
 	if err != nil {
 		t.Fatal(err)
 	}
 	want := 3
 	got := c.Lines()
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestWords(t *testing.T) {
+	t.Parallel()
+	inputBuf := bytes.NewBufferString("1\n2 words\n3 this time")
+	c, err := counter.NewCounter(
+		counter.WithInput(inputBuf),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := 6
+	got := c.Words()
+	if want != got {
+		t.Errorf("want %d, got %d", got, want)
+	}
+}
+
+func TestWordCount(t *testing.T) {
+	args := []string{"-w", "testdata/three_lines.txt"}
+	c, err := counter.NewCounter(
+		counter.FromArgs(args),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := 3
+	got := c.Words()
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
 	}
